97763286420ccdaeb36db211c8db5fd287620409
Solomon Reinman <reinman@goldenhelix.com>
2026-01-16 17:06:54 -0700

    Make directory prior to copying templates in install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
+mkdir -p "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
+
 cp -f ./.project_templates/*.vsproject-template "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
+
+echo "Installed VarSeq project templates"
