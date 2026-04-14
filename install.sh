fa3253ae8aa92cf993ba7eafd804b79d7740c772
Solomon Reinman <reinman@goldenhelix.com>
2026-04-14 14:57:31 -0600

    Add submodule pull to install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ mkdir -p "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
 
 cp -f ./.project_templates/*.vsproject-template "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
 
+git submodule update --init --recursive
+
 echo "Installed VarSeq project templates"
