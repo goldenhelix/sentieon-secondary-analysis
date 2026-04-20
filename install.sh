d373fde68bb6c02ed88e9778a644153f474766a6
Solomon Reinman <reinman@goldenhelix.com>
2026-04-20 13:09:50 -0600

    Update install.sh to use correct CWD

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
 mkdir -p "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
 
 cp -f ./.project_templates/*.vsproject-template "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
