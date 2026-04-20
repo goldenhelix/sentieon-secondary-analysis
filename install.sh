b48332796d12bbd07bec4244d3912c84ac8cff66
Solomon Reinman <reinman@goldenhelix.com>
2026-04-20 13:11:33 -0600

    Add additional output to install script

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+set -x
 cd "$(dirname "$0")"
+
+echo "Working directory: $(pwd)"
+echo "Git version: $(git --version 2>&1)"
+echo "Git repo root: $(git rev-parse --show-toplevel 2>&1)"
 
 mkdir -p "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
 
 cp -f ./.project_templates/*.vsproject-template "${WORKSPACE_DIR}/AppData/VarSeq/User Data/ProjectTemplates"
 
 git submodule update --init --recursive
+echo "Submodule update exit code: $?"
+
+echo "Submodule status:"
+git submodule status
 
 echo "Installed VarSeq project templates"
